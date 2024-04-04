e9573dbef55f254cf3c9808cbadd6208caa17ba0
percheta <aubin.perchet@etu.umonte>
2024-04-04 11:28:45 +0200

    carte

--- src/main/java/fr/umontpellier/iut/trains/cartes/CabineDuConducteur.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/CabineDuConducteur.java
@@ -2,6 +2,6 @@ package fr.umontpellier.iut.trains.cartes;
 
 public class CabineDuConducteur extends Carte {
     public CabineDuConducteur() {
-        super("Cabine du conducteur");
+        super("Cabine du conducteur", 2, 0, CouleurCarte.ROUGE);
     }
 }
