4dc9bf9e090cf82f696ced76335fbca5324d85d1
percheta <aubin.perchet@etu.umonte>
2024-04-04 12:52:04 +0200

    EYTAN LE

--- src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
@@ -1,11 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Aiguillage extends Carte {
     public Aiguillage() {
         super("Aiguillage", 5, 0, CouleurCarte.ROUGE);
     }
 
-    public int getPrix() {
-        return 5;
+    @Override
+    public void jouer(Joueur j) {
+        j.piocher(2);
     }
 }
