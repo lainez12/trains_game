bc7c11dbd1640c88a2b3ce12d682758b06b9788e
Eytan Lacombe <eytan.lacombe@etu.umontpellier.fr>
2024-04-05 13:36:21 +0200

    voisouterre

--- src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
@@ -1,5 +1,10 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aiguillage extends Carte {
     public Aiguillage() {
         super("Aiguillage", 5, 0, CouleurCarte.ROUGE);
@@ -7,5 +12,13 @@ public class Aiguillage extends Carte {
 
     public int getPrix() {
         return 5;
+    }
+    @Override
+    public void jouer(Joueur j){
+        List<Carte> cartepiocher = new ListeDeCartes();
+        cartepiocher = j.piocher(2);
+        for (int i = 0; i < cartepiocher.size(); i++) {
+            j.getMain().add(cartepiocher.get(i));
+        }
     }
 }
--- src/main/java/fr/umontpellier/iut/trains/cartes/TrainOmnibus.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TrainOmnibus.java
@@ -1,7 +1,15 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class TrainOmnibus extends Carte {
     public TrainOmnibus() {
         super("Train omnibus", 1, 1, CouleurCarte.BLEU);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+        joueur.setArgent(joueur.getArgent() + 1);
     }
 }
