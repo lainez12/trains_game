ad9109d17c6bf792539b77ae1ac9490bf297f6c0
Eytan Lacombe <eytan.lacombe@etu.umontpellier.fr>
2024-04-05 15:47:13 +0200

    ajout traindirect

--- src/main/java/fr/umontpellier/iut/trains/cartes/TrainDirect.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TrainDirect.java
@@ -1,7 +1,15 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class TrainDirect extends Carte {
     public TrainDirect() {
         super("Train direct", 6, 3, CouleurCarte.BLEU);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+        joueur.setArgent(joueur.getArgent() + 3);
     }
 }
