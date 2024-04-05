b12203fb0f0475a2b21ed6a4abf76bb9d304cc24
Eytan Lacombe <eytan.lacombe@etu.umontpellier.fr>
2024-04-05 15:47:25 +0200

    ajout trainexpress

--- src/main/java/fr/umontpellier/iut/trains/cartes/TrainExpress.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TrainExpress.java
@@ -1,7 +1,15 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class TrainExpress extends Carte {
     public TrainExpress() {
         super("Train express", 3, 2, CouleurCarte.BLEU);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+        joueur.setArgent(joueur.getArgent() + 2);
     }
 }
