26f5a8fc9881e634af2d8f6b109ae7bb9336760c
percheta <aubin.perchet@etu.umonte>
2024-04-18 14:23:01 +0200

    Add CentreDeControle

--- src/main/java/fr/umontpellier/iut/trains/cartes/CentreDeControle.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/CentreDeControle.java
@@ -1,7 +1,22 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class CentreDeControle extends Carte {
     public CentreDeControle() {
         super("Centre de contrôle", 3, 0, CouleurCarte.ROUGE);
+    }
+
+    @Override
+    public void jouer(Joueur j) {
+        Carte c = j.piocher();
+        if (c != null) {
+            j.getMain().add(c);
+            j.log("Nommez la prochaine carte de votre deck : ");
+            String s = j.getJeu().lireLigne();
+            if (j.getPioche().get(0).getNom() == s) {
+                j.getMain().add(j.piocher());
+            }
+        }
     }
 }
