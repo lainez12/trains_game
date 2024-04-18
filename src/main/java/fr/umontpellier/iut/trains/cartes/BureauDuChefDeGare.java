53b6c2e39d46d06cf26193d9377961bc00e98730
percheta <aubin.perchet@etu.umonte>
2024-04-18 14:22:34 +0200

    Add BureauDuChefDeGare

--- src/main/java/fr/umontpellier/iut/trains/cartes/BureauDuChefDeGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/BureauDuChefDeGare.java
@@ -1,7 +1,29 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import fr.umontpellier.iut.trains.Joueur;
+
 public class BureauDuChefDeGare extends Carte {
     public BureauDuChefDeGare() {
         super("Bureau du chef de gare", 4, 0, CouleurCarte.ROUGE);
+    }
+
+    @Override
+    public void jouer(Joueur j) {
+        Set<String> choix = new HashSet<>();
+        String res = "";
+        for (Carte i : j.getMain()) {
+            if (i.getCouleur() == CouleurCarte.ROUGE) {
+                choix.add(i.getNom());
+            }
+        }
+        if (choix.isEmpty()) {
+            System.out.println("Vous n'avez pas de carte ACTION en main");
+        } else {
+            res = j.choisir("Choisissez une carte ACTION de votre main à révéler", choix, null, false);
+        }
+        j.getMain().getCarte(res).jouer(j);
     }
 }
