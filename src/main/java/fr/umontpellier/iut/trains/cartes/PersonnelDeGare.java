df58ba2258d38d67aa8b76219b2e22f3008ef421
eytanlacombe <eytanlacombe29@yahoo.com>
2024-04-21 16:58:19 +0200

    perssonel de gare

--- src/main/java/fr/umontpellier/iut/trains/cartes/PersonnelDeGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PersonnelDeGare.java
@@ -1,7 +1,41 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class PersonnelDeGare extends Carte {
     public PersonnelDeGare() {
         super("Personnel de gare", 2, 0, CouleurCarte.ROUGE);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+
+        Set<String> choix = new HashSet<>();
+        String rep = "";
+        choix.add("piocher");
+        choix.add("argent");
+        choix.add("ferraille");
+
+        rep = joueur.choisir("Choisisser entre piochez une carte, gagner 1 d'argent ou remetre 1 de feraille de votre main sur la pile feraille",choix,null,false);
+
+        if (rep.equals("piocher")){
+            Carte temp = joueur.piocher();
+            if (temp != null){
+                joueur.getMain().add(temp);
+            }
+        }
+        else if (rep.equals("argent")){
+            joueur.setArgent(joueur.getArgent() + 1);
+        }
+        else if (rep.equals("ferraille")){
+            if (joueur.getMain().count("Ferraille") != 0){
+                Ferraille f = new Ferraille();
+                joueur.getJeu().getReserve().get("Ferraille").add(joueur.getMain().retirer("Ferraille"));
+            }
+        }
     }
 }
