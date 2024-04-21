898c332ffc594c2a6b1b574208f882eb482126d8
percheta <aubin.perchet@etu.umonte>
2024-04-21 18:19:15 +0200

    Add Viaduc

--- src/main/java/fr/umontpellier/iut/trains/cartes/Viaduc.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Viaduc.java
@@ -1,7 +1,21 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Viaduc extends Carte {
     public Viaduc() {
         super("Viaduc", 5, 0, CouleurCarte.VERT);
+    }
+
+    @Override
+    public void jouer(Joueur j) {
+        j.setPointsRails(j.getPointsRails() + 1);
+        if (!Joueur.estEnJeu(j.getCartesEnJeu(), Depotoir.class)) {
+            Carte c = j.getJeu().prendreDansLaReserve("Ferraille");
+            if (c != null) {
+                j.getMain().add(c);
+            }
+        }
+        j.getCartesEnJeu().add(this);
     }
 }
