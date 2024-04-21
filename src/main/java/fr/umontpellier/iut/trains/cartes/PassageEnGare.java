1e7dcded521ff6398f62760ae878d7a804eb3497
eytanlacombe <eytanlacombe29@yahoo.com>
2024-04-21 17:04:07 +0200

    passage en gare

--- src/main/java/fr/umontpellier/iut/trains/cartes/PassageEnGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PassageEnGare.java
@@ -1,7 +1,19 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class PassageEnGare extends Carte {
     public PassageEnGare() {
         super("Passage en gare", 3, 1, CouleurCarte.ROUGE);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+        Carte temp = joueur.piocher();
+        if (temp != null){
+            joueur.getMain().add(temp);
+        }
+        joueur.setArgent(joueur.getArgent() + 1);
     }
 }
