30ae4782354a01c0ca7c892da935f675153b76b4
percheta <aubin.perchet@etu.umonte>
2024-04-21 18:20:30 +0200

    Add PontEnAcier

--- src/main/java/fr/umontpellier/iut/trains/cartes/PontEnAcier.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PontEnAcier.java
@@ -1,7 +1,21 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class PontEnAcier extends Carte {
     public PontEnAcier() {
         super("Pont en acier", 4, 0, CouleurCarte.VERT);
+    }
+
+    @Override
+    public void jouer(Joueur j) {
+        j.setPointsRails(j.getPointsRails() + 1);
+        if (!Joueur.estEnJeu(j.getCartesEnJeu(), Depotoir.class)) {
+            Carte c = j.getJeu().prendreDansLaReserve("Ferraille");
+            if (c != null) {
+                j.getMain().add(c);
+            }
+        }
+        j.getCartesEnJeu().add(this);
     }
 }
