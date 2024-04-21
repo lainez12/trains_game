9046c5822c233a38bbc64c501d383b8265ebe27d
eytanlacombe <eytanlacombe29@yahoo.com>
2024-04-21 16:05:15 +0200

    remorquage

--- src/main/java/fr/umontpellier/iut/trains/cartes/Remorquage.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Remorquage.java
@@ -1,7 +1,33 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Remorquage extends Carte {
     public Remorquage() {
         super("Remorquage", 3, 0, CouleurCarte.ROUGE);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+        Set<String> choix = new HashSet<>();
+        String rep = "";
+        for (int i = 0; i < joueur.getDefausse().size(); i++) {
+            if (joueur.getDefausse().get(i).getCouleur() == CouleurCarte.BLEU) {
+                choix.add(joueur.getDefausse().get(i).getNom());
+            }
+        }
+        if (choix.isEmpty()){
+            rep = joueur.choisir("Vous devez passer car vous n'avez pas de carte train dans votre defausse", choix, null, true);
+        }
+        else {
+            rep = joueur.choisir("Choisissez une carte Train de votre main pour l'écarter", choix, null, false);
+            Carte temp = joueur.getDefausse().getCarte(rep);
+            joueur.getDefausse().retirer(rep);
+            joueur.getMain().add(temp);
+        }
     }
 }
