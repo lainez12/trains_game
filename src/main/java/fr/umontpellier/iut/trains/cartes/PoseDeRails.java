8f5086ebf013ec7971e7e1b52786d0dbfc14c893
eytanlacombe <eytanlacombe29@yahoo.com>
2024-04-21 16:05:03 +0200

    pose de rail

--- src/main/java/fr/umontpellier/iut/trains/cartes/PoseDeRails.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PoseDeRails.java
@@ -1,7 +1,16 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class PoseDeRails extends Carte {
     public PoseDeRails() {
         super("Pose de rails", 3, 0, CouleurCarte.VERT);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+        joueur.setPointsRails(joueur.getPointsRails() + 1);
+        joueur.getCartesRecues().add(joueur.getJeu().prendreDansLaReserve("Ferraille"));
     }
 }
