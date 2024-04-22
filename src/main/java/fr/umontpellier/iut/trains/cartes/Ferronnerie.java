e0632be09efb28a523144cff60b40234d4aaf88f
percheta <aubin.perchet@etu.umonte>
2024-04-22 21:12:16 +0200

    Add Ferronnerie

--- src/main/java/fr/umontpellier/iut/trains/cartes/Ferronnerie.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Ferronnerie.java
@@ -1,7 +1,15 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Ferronnerie extends Carte {
     public Ferronnerie() {
         super("Ferronnerie", 4, 1, CouleurCarte.ROUGE);
+    }
+
+    @Override
+    public void jouer(Joueur j) {
+        j.setArgent(j.getArgent() + getValeur());
+        j.getPassifsEnJeu().add(this);
     }
 }
