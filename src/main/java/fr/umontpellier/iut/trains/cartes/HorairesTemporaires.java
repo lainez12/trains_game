cddfffe2b82573270966a2796bb0315b8b6e8f95
percheta <aubin.perchet@etu.umonte>
2024-04-22 21:13:13 +0200

    Add HraireTemporaires (nn fonctionnelle

--- src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
@@ -1,7 +1,30 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import java.util.ArrayList;
+
+import fr.umontpellier.iut.trains.Joueur;
+
 public class HorairesTemporaires extends Carte {
     public HorairesTemporaires() {
         super("Horaires temporaires", 5, 0, CouleurCarte.ROUGE);
+    }
+
+    @Override
+    public void jouer(Joueur j) {
+        int count = 0;
+        Carte c;
+        while (count != 2) {
+            c = j.piocher();
+            if (c != null) {
+                if (c.getCouleur() == CouleurCarte.BLEU) {
+                    j.getMain().add(c);
+                    count++;
+                } else {
+                    j.getDefausse().add(c);
+                }
+            } else {
+                break;
+            }
+        }
     }
 }
